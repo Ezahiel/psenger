d61b7be36533b21bf9cc744b60ebe8caa08a597d
Philip A Senger <philip.a.senger@cngrgroup.com>
2020-08-11 15:41:42 +1000

    ## Changed
    * Moved code arround for liniting purpose

--- scrape.js
+++ scrape.js
@@ -1,4 +1,3 @@
-
 const { Octokit } = require("@octokit/rest");
 
 /**
@@ -13,42 +12,46 @@ const REPO_DETAILS = {
     owner: process.env.GITHUB_REPOSITORY_OWNER,
     repo: process.env.GITHUB_REPOSITORY_OWNER,
 };
-
 const START_COMMENT = "<!--START_SECTION:endorsements-->";
 const END_COMMENT = "<!--END_SECTION:endorsements-->";
 const listReg = new RegExp(`${START_COMMENT}[\\s\\S]+${END_COMMENT}`);
-
-// console.log({ REPO_DETAILS });
-// console.log("ghtoken", process.env.ENV_GITHUB_TOKEN);
-
 const octokit = new Octokit({ auth: `token ${process.env.ENV_GITHUB_TOKEN}` });
 
-(async function main() {
-    const readme = await getReadme(octokit);
-    let oldFences = listReg.exec(readme.content)
-    oldFences = oldFences && oldFences[0] // could be null
-    const data = await getReactions();
-    try {
-        let listWithFences = generateStuffInsideFences(data, readme.content);
-        if (listWithFences === oldFences) {
-            console.log('NO CHANGE detected in the endorsements, skipping commit')
-            return
-        }
-        let newContents = readme.content.replace(listReg, listWithFences);
-        await octokit.repos.createOrUpdateFileContents({
-            ...REPO_DETAILS,
-            content: Buffer.from(newContents).toString("base64"),
-            path: "README.md",
-            message: `endorsements ${new Date().toISOString()}`,
-            sha: readme.sha,
-            branch: "master",
-        });
-    } catch (err) {
-        console.error(err);
-    }
-})();
+// async function getReadme (octokit: Octokit) {
+const getReadme = async function getReadme(octokit) {
+    const res = await octokit.repos.getReadme(REPO_DETAILS);
+    const encoded = res.data.content;
+    const decoded = Buffer.from(encoded, "base64").toString("utf8");
+    return {
+        content: decoded,
+        sha: res.data.sha,
+    };
+}
 
-function generateStuffInsideFences(data) {
+// async function to get reactions
+const getReactions = async function getReactions() {
+    let { data } = await octokit.issues.listForRepo(REPO_DETAILS);
+    data = data
+        .filter((x) => x.title.startsWith("Endorse: "))
+        .map((x) => ({ ...x, title: x.title.slice(9) }));
+    data = await Promise.all(
+        data.map(async (x) => {
+            const reaction = await octokit.reactions.listForIssue({
+                ...REPO_DETAILS,
+                issue_number: x.number,
+            });
+            return {
+                title: x.title,
+                url: x.html_url,
+                number: x.number,
+                reactions: reaction.data, // an array of USER
+            };
+        })
+    );
+    return data; // custom object
+}
+
+const generateStuffInsideFences = function generateStuffInsideFences(data) {
     console.log('data=', JSON.stringify(data, null, 4));
     const renderedList = data
         .map(
@@ -78,36 +81,31 @@ I would like to add you to my professional network on the GITHUB.
   <a href="https://github.com/psenger/psenger/issues/new?assignees=&labels=&template=endorsement-template.md&title=Endorse%3A+SKILL_HERE">Endorse me for a new skill</a>
   ${END_COMMENT}`;
     return listWithFences
-}
+};
 
-// async function getReadme (octokit: Octokit) {
-async function getReadme(octokit) {
-    const res = await octokit.repos.getReadme(REPO_DETAILS);
-    const encoded = res.data.content;
-    const decoded = Buffer.from(encoded, "base64").toString("utf8");
-    return {
-        content: decoded,
-        sha: res.data.sha,
-    };
-}
-async function getReactions() {
-    let { data } = await octokit.issues.listForRepo(REPO_DETAILS);
-    data = data
-        .filter((x) => x.title.startsWith("Endorse: "))
-        .map((x) => ({ ...x, title: x.title.slice(9) }));
-    data = await Promise.all(
-        data.map(async (x) => {
-            const reaction = await octokit.reactions.listForIssue({
-                ...REPO_DETAILS,
-                issue_number: x.number,
-            });
-            return {
-                title: x.title,
-                url: x.html_url,
-                number: x.number,
-                reactions: reaction.data, // an array of USER
-            };
-        })
-    );
-    return data; // custom object
-}
+(async function main() {
+    const readme = await getReadme(octokit);
+    let oldFences = listReg.exec(readme.content)
+    oldFences = oldFences && oldFences[0] // could be null
+    const data = await getReactions();
+    try {
+        let listWithFences = generateStuffInsideFences(data, readme.content);
+        if (listWithFences === oldFences) {
+            console.log('NO CHANGE detected in the endorsements, skipping commit')
+            return
+        }
+        let newContents = readme.content.replace(listReg, listWithFences);
+        await octokit.repos.createOrUpdateFileContents({
+            ...REPO_DETAILS,
+            content: Buffer.from(newContents).toString("base64"),
+            path: "README.md",
+            message: `endorsements ${new Date().toISOString()}`,
+            sha: readme.sha,
+            branch: "master",
+        });
+    } catch (err) {
+        console.error(err);
+    }
+})();
+
+
